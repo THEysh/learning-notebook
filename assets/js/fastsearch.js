10971430a5512d8908315fc24f4156f9a86253d6
THEysh <1084701312@qq.com>
2024-11-13 21:51:30 +0800

    1

--- assets/js/fastsearch.js
+++ assets/js/fastsearch.js
@@ -48,7 +48,7 @@ window.onload = function () {
             }
         }
     };
-    xhr.open('GET', "https://learning-notebook.netlify.app/index.json");
+    xhr.open('GET', "../index.json");
     xhr.send();
 }
 
